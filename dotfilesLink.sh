2f1711cc718906bcb0d8cbecd54a6b21a56387cc
hamakou108 <kosuke1602177mteitm19@gmail.com>
2017-12-11 22:03:28 +0900

    Update creating symbolic link to dotfiles

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.dein.toml ~/.dein.toml
-ln -sf ~/dotfiles/.dein_lazy.toml ~/.dein_lazy.toml
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zprofile ~/.zprofile
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ${PWD}/.vimrc ~/.vimrc
+ln -sf ${PWD}/.dein.toml ~/.dein.toml
+ln -sf ${PWD}/.dein_lazy.toml ~/.dein_lazy.toml
+ln -sf ${PWD}/.bashrc ~/.bashrc
+ln -sf ${PWD}/.bash_profile ~/.bash_profile
+ln -sf ${PWD}/.zshrc ~/.zshrc
+ln -sf ${PWD}/.zprofile ~/.zprofile
+ln -sf ${PWD}/.tmux.conf ~/.tmux.conf
