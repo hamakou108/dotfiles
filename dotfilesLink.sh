03640f1634f34f8e524de0aaf63b8590404870fa
hamakou108 <kosuke1602177mteitm19@gmail.com>
2017-12-18 13:49:50 +0000

    Update scripts to create links

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,3 +5,5 @@ ln -sf ${PWD}/.dein_lazy.toml ~/.dein_lazy.toml
 ln -sf ${PWD}/.bashrc ~/.bashrc
 ln -sf ${PWD}/.zshrc ~/.zshrc
 ln -sf ${PWD}/.tmux.conf ~/.tmux.conf
+touch ~/.bash_profile
+touch ~/.zprofile
