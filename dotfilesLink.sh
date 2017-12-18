0d2cbb9a5fbb8480b9f1d4bf0b52814d81b19c15
Kosuke Hamada <hamada.kosuke@nifty.co.jp>
2017-12-18 11:50:04 +0900

    Remove .profile

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,7 +3,5 @@ ln -sf ${PWD}/.vimrc ~/.vimrc
 ln -sf ${PWD}/.dein.toml ~/.dein.toml
 ln -sf ${PWD}/.dein_lazy.toml ~/.dein_lazy.toml
 ln -sf ${PWD}/.bashrc ~/.bashrc
-ln -sf ${PWD}/.bash_profile ~/.bash_profile
 ln -sf ${PWD}/.zshrc ~/.zshrc
-ln -sf ${PWD}/.zprofile ~/.zprofile
 ln -sf ${PWD}/.tmux.conf ~/.tmux.conf
