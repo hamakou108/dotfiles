0ecaa59dde827a343c9c5fe97d150735289ef0cd
hamakou108 <kosuke1602177mteitm19@gmail.com>
2017-12-21 22:28:13 +0900

    Change filenames and Update symbolic links

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-ln -sf ${PWD}/.vimrc ~/.vimrc
-ln -sf ${PWD}/.dein.toml ~/.dein.toml
-ln -sf ${PWD}/.dein_lazy.toml ~/.dein_lazy.toml
-ln -sf ${PWD}/.bashrc ~/.bashrc
-ln -sf ${PWD}/.zshrc ~/.zshrc
-ln -sf ${PWD}/.tmux.conf ~/.tmux.conf
+ln -sf ${PWD}/vimrc ~/.vimrc
+ln -sf ${PWD}/dein.toml ~/.dein.toml
+ln -sf ${PWD}/dein_lazy.toml ~/.dein_lazy.toml
+ln -sf ${PWD}/bashrc ~/.bashrc
+ln -sf ${PWD}/zshrc ~/.zshrc
+ln -sf ${PWD}/tmux.conf ~/.tmux.conf
 touch ~/.bash_profile
 touch ~/.zprofile
