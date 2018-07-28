198a3c16f56c48989f341eaea233e14bee726a42
hamakou108 <kosuke1602177mteitm19@gmail.com>
2018-07-28 18:17:09 +0900

    Remove duplicate command to create a link

--- bin/link.sh
+++ bin/link.sh
@@ -3,8 +3,7 @@
 # create links to configuration files
 dotfiles_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); cd ../; pwd)
 ln -nsf ${dotfiles_dir}/config/nvim ~/.config/
-ln -nsf ~/.config/nvim ~/.vim
-ln -sf ~/.config/nvim/init.vim ~/.vimrc
+ln -nsf ${dotfiles_dir}/config/nvim ~/.vim
 ln -sf ${dotfiles_dir}/config/nvim/init.vim ~/.vimrc
 ln -sf ${dotfiles_dir}/config/zsh/zshrc ~/.zshrc
 ln -sf ${dotfiles_dir}/config/tmux/tmux.conf ~/.tmux.conf
