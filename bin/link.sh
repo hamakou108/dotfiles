fffa12b68f91c344dec3bfc35f089946fdbbcf13
hamakou108 <kosuke1602177mteitm19@gmail.com>
2018-09-25 23:16:33 +0900

    Add and Remove settings for introducing Oni

--- bin/link.sh
+++ bin/link.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # create links to configuration files
 dotfiles_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); cd ../; pwd)
 ln -nsf ${dotfiles_dir}/config/nvim ~/.config/
 ln -nsf ${dotfiles_dir}/config/nvim ~/.vim
 ln -sf ${dotfiles_dir}/config/nvim/init.vim ~/.vimrc
+ln -sf ${dotfiles_dir}/config/nvim/oni/config.tsx ~/.config/oni/config.tsx
 ln -sf ${dotfiles_dir}/config/zsh/zshrc ~/.zshrc
 ln -sf ${dotfiles_dir}/config/tmux/tmux.conf ~/.tmux.conf
 
