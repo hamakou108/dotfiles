ef85a2ca9aff0588425c335f9dc1bab4aba3e863
hamakou108 <hamakou108@gmail.com>
2023-02-18 23:48:57 +0900

    fix: cleaned up commands for missing files

--- bin/link.sh
+++ bin/link.sh
@@ -30,8 +30,6 @@ make_link ${dotfiles_dir}/config/zsh/zshrc         ~/.zshrc
 make_link ${dotfiles_dir}/config/fish/config.fish  ~/.config/fish/config.fish
 make_link ${dotfiles_dir}/config/fish/fish_plugins ~/.config/fish/fish_plugins
 make_link ${dotfiles_dir}/config/tmux/tmux.conf    ~/.tmux.conf
-make_link ${dotfiles_dir}/config/oni/config.tsx    ~/.config/oni/config.tsx
-make_link ${dotfiles_dir}/config/hyper/hyper.js    ~/.hyper.js
 
 # update profiles
 touch ~/.bash_profile
