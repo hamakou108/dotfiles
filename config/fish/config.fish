ae1e36c97ae29c5b9119bf709a2ed50433ff8e7a
hamakou108 <hamakou108@gmail.com>
2023-02-19 10:41:47 +0900

    feat(fish): apple silicon support

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -19,54 +19,65 @@ alias mkdir='mkdir -p'
 alias sudo='sudo '
 
 ################################################################################
-# Fisher
-# After fisher command has been sourced, run `fisher update` manually. Don't
-# write `fisher update` in this file, or Fish would get into infinite loop.
-if ! type -q fisher
-    curl -sL https://git.io/fisher | source
-end
+# Homebrew
+if command -q which brew
+    set -x HOMEBREW_PREFIX (brew --prefix)
 
-################################################################################
-# Tmux
-alias tmux='tmux -2'
-set -x TERM screen-256color
-
-################################################################################
-# SSH
-alias ssh='set -x TERM screen ssh'
-
-################################################################################
-# NeoVim
-# alias
-if type -q nvim
-    alias vi=(which nvim)
-    alias vim=(which nvim)
-    alias editor=(which nvim)
-    function vimdiff
-        set -l nvim_path (which nvim)
-        command $nvim_path -d $argv
+    # for Apple Silicon
+    switch (uname -m)
+    case "arm64"
+        fish_add_path $HOMEBREW_PREFIX/bin
     end
+
+    ################################################################################
+    # Fisher
+    # After fisher command has been sourced, run `fisher update` manually. Don't
+    # write `fisher update` in this file, or Fish would get into infinite loop.
+    if ! type -q fisher
+        curl -sL https://git.io/fisher | source
+    end
+
+    ################################################################################
+    # Tmux
+    alias tmux='tmux -2'
+    set -x TERM screen-256color
+
+    ################################################################################
+    # SSH
+    alias ssh='set -x TERM screen ssh'
+
+    ################################################################################
+    # NeoVim
+    # alias
+    if type -q nvim
+        alias vi=(which nvim)
+        alias vim=(which nvim)
+        alias editor=(which nvim)
+        function vimdiff
+            set -l nvim_path (which nvim)
+            command $nvim_path -d $argv
+        end
+    end
+
+    # XDG_CONFIG_HOME
+    set -x XDG_CONFIG_HOME $HOME/.config
+
+    ################################################################################
+    # GNU command utilities
+    fish_add_path $HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin
+    fish_add_path $HOMEBREW_PREFIX/opt/ed/libexec/gnubin
+    fish_add_path $HOMEBREW_PREFIX/opt/findutils/libexec/gnubin
+    fish_add_path $HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin
+    fish_add_path $HOMEBREW_PREFIX/opt/gnu-tar/libexec/gnubin
+    fish_add_path $HOMEBREW_PREFIX/opt/grep/libexec/gnubin
+
+    if test -d $HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin
+        alias ls='ls -F --color=auto'
+        alias la='ls -aF --color=auto'
+        alias ll='ls -lF --color=auto'
+    end
+
+    ################################################################################
+    # asdf
+    source $HOMEBREW_PREFIX/opt/asdf/libexec/asdf.fish
 end
-
-# XDG_CONFIG_HOME
-set -x XDG_CONFIG_HOME $HOME/.config
-
-################################################################################
-# GNU command utilities
-fish_add_path /usr/local/opt/coreutils/libexec/gnubin
-fish_add_path /usr/local/opt/ed/libexec/gnubin
-fish_add_path /usr/local/opt/findutils/libexec/gnubin
-fish_add_path /usr/local/opt/gnu-sed/libexec/gnubin
-fish_add_path /usr/local/opt/gnu-tar/libexec/gnubin
-fish_add_path /usr/local/opt/grep/libexec/gnubin
-
-if test -d /usr/local/opt/coreutils/libexec/gnubin
-    alias ls='ls -F --color=auto'
-    alias la='ls -aF --color=auto'
-    alias ll='ls -lF --color=auto'
-end
-
-################################################################################
-# asdf
-source /usr/local/opt/asdf/libexec/asdf.fish
-
